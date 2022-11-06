dbde8cbedc9bec8f53afc002f686206fa9815161
Suvam Roy <81362876+sroy1999-dev@users.noreply.github.com>
2022-11-06 22:08:30 +0530

    C:\Figma\Project7\Fitness

--- fitness.js
+++ fitness.js
@@ -1,5 +1,22 @@
-var btn = document.getElementById('btn');
-var popup = document.getElementsByClassName('popup');
-btn.addEventListener('click', () => {
-    popup.style.display = 'block';
-})
+let button = document.getElementById('btn');
+if(button){
+    button.addEventListener('click', () => {
+        document.querySelector('.popup').style.display = 'flex';
+        document.querySelector('.popup').style.position = 'fixed';
+        document.getElementById('poster').style.display = 'hidden';
+    })
+}
+
+let cancel = document.querySelector('.cancel');
+if(cancel) {
+    cancel.addEventListener('click', () => {
+        document.querySelector('.popup').style.display = 'none';
+    })
+}
+
+let toTop = document.getElementById('top');
+if(toTop){
+    toTop.addEventListener('click', () => {
+        window.scrollTo({top: 0, behavior: "smooth"});
+    })
+}
